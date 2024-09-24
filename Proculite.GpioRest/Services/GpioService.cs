96c7e17da0066e712fb2d29bc1b68f27aabeac12
Filip Strajnar <filip.strajnar@gmail.com>
2024-09-24 23:44:41 +0200

    Pin setup.

--- Proculite.GpioRest/Services/GpioService.cs
+++ Proculite.GpioRest/Services/GpioService.cs
@@ -1,11 +1,46 @@
+using System.Device.Gpio;
+
 namespace Proculite.GpioRest.Services
 {
     public class GpioService
     {
         public readonly ILogger<GpioService> _logger;
-        public GpioService(ILogger<GpioService> logger)
+        private readonly int[] _pins;
+        private readonly GpioController _gpioController;
+
+        public GpioService(ILogger<GpioService> logger, IConfiguration configuration)
         {
             _logger = logger;
+            _pins = configuration
+                .GetSection("Pins")
+                .GetChildren()
+                .Select(pin => pin.Get<int>())
+                .ToArray();
+
+            _gpioController = new GpioController();
+            SetupPins();
+        }
+
+        private void SetupPins()
+        {
+            _logger.LogInformation("Setting up {PinCount} pins", _pins.Length);
+            PinMode pinMode = PinMode.Input;
+
+            foreach (var pin in _pins)
+            {
+                if (_gpioController.IsPinOpen(pin))
+                {
+                    _logger.LogWarning("Pin number {Pin} is already open.", pin);
+                    continue;
+                }
+
+                if (!_gpioController.IsPinModeSupported(pin, pinMode))
+                {
+                    _logger.LogWarning("Pin number {Pin} does not support {PinMode}", pin, pinMode);
+                }
+
+                _gpioController.OpenPin(pin, pinMode, PinValue.Low);
+            }
         }
     }
 }
